91cced2cbdcde4c5e20fd9514fe8c9cf2d5b24a6
Paulo Garcia <paulo.garcia@tecnico.ulisboa.pt>
2015-05-06 15:30:13 +0100

    bug fix on print-object tensor is now a class instead of a structure

--- apl_clos.lisp
+++ apl_clos.lisp
@@ -1,25 +1,64 @@
- (defstruct (tensor (:print-object print-object-tensor))
- 					values	
- 					rank	
- 					shape)
+(defclass tensor ()
+ 	((values 
+ 		:initarg :values
+ 		:reader tensor-values)	
+ 	 (rank
+ 	 	:initarg :rank
+ 	 	:reader tensor-rank)
+ 	 (shape
+ 	 	:initarg :shape
+ 	 	:reader tensor-shape)))
 
 (defun s (arg)
-	(make-tensor :values (make-array nil :initial-contents arg)
-				 :rank 0
-				 :shape 0))
+	(make-instance 'tensor :values (make-array nil :initial-contents arg)
+				   		   :rank 0
+				           :shape '()))
 
-(defun v (&rest args)
-	(let ((shape (list (list-length args))))
-		(make-tensor :values (make-array shape :initial-contents args)
-					 :rank 1
-					 :shape shape)))
+   ; (defun v (&rest args)
+   ; 	(let ((shape (list (list-length args))))
+   ; 		(make-instance 'tensor :values (make-array shape :initial-contents args)
+   ; 					 		   :rank 1
+   ; 					 		   :shape shape)))
 
+   (defun v (&rest args)
+   	(let ((shape (list (list-length args))))
+   		(make-instance 'tensor :values (make-array '(2 2 2 2) :initial-contents '((((1 1) (1 1)) ((1 1) (1 1))) (((1 1) (1 1)) ((1 1) (1 1)))))
+   					 		   :rank 4
+   					 		   :shape '(2 2 2 2))))
 
-(defun print-object-tensor ((object tensor) stream)
+   ; (defun v (&rest args)
+   ; 	(let ((shape (list (list-length args))))
+   ; 		(make-instance 'tensor :values (make-array '(2 2) :initial-contents '((1 1) (1 1)))
+   ; 					 		   :rank 2
+   ; 					 		   :shape '(2 2))))
+
+(defmethod print-object ((object tensor) stream)
 	(tensor-print stream (tensor-values object) (tensor-rank object) (tensor-shape object) '()))
 
 (defun tensor-print (stream values rank shape indexes)
-	(if (eq (list-length indexes) rank)
+	(if (eq shape nil)
 		(format stream "~s " (apply #'aref values indexes))
-		(dotimes (dim (car shape))
-			(tensor-print stream values rank (cdr shape) (append indexes (list dim))))))	
+		(progn (dotimes (dim (car shape))
+			   		(tensor-print stream values rank (cdr shape) (append indexes (list dim))))
+				(if (not (eq rank (list-length shape))) 
+					(if (not (eql (car (last indexes)) (- (car (last shape)) 1)))
+						(dotimes (r (list-length shape))
+							(format stream "~%")))))))
+
+(defun tensor-apply (values rank shape indexes))
+
+(defun .- (arg ))
+
+(defun ./ (arg))
+
+(defun .! (arg))
+
+(defun .sin (arg))
+
+(defun .cos (arg))
+
+(defun .not (arg))
+
+(defun shape (arg))
+
+(defun interval (arg))
